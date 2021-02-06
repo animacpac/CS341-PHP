da22c0cb40a28940d4e2a976605263c04162d0ee
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-05 22:07:31 -0700

    Update scripture_details.php

--- web/scripture_details.php
+++ web/scripture_details.php
@@ -29,7 +29,7 @@
         <th>Address</th>
     </tr>
     <tr>
-        <td><?php echo $row->first_name; ?></td>
+        <td><?php echo $row['first_name']; ?></td>
         <td><?php echo $row->last_name; ?></td>
         <td><?php echo $row->userid; ?></td>
         <td><?php echo $row->content; ?></td>
