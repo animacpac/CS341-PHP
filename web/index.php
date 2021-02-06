eae1915f6d63ee2110691ccf2a082929aa5b4885
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-05 22:59:21 -0700

    update

--- web/index.php
+++ web/index.php
@@ -15,22 +15,22 @@ session_start();
     <form method="post" action="results.php">
     <p>Choose the user</p>
     <label>Users </label>
-    <select name='first_name_filter' id='first_name_filter' required>
+    <select name='last_name_filter' id='last_name_filter' required>
     <?php
        $names_array = array();
        
-       foreach ($db->query ('SELECT DISTINCT first_name FROM note') as $row )
+       foreach ($db->query ('SELECT DISTINCT last_name FROM note') as $row )
        {
            //Add book to the book array if it has not been added 
-          if(!(in_array($row['first_name'], $names_array)))
+          if(!(in_array($row['last_name'], $names_array)))
           {
-              array_push($names_array, $row['first_name']);
+              array_push($names_array, $row['last_name']);
           }
        }
        //loop the books_array variable to create an option for each book 
-       foreach ($names_array as $first_name_in_array )
+       foreach ($names_array as $last_name_in_array )
        {
-        echo '<option value="' . $first_name_in_array . '">' .  $first_name_in_array . '</option>';
+        echo '<option value="' . $last_name_in_array . '">' .  $last_name_in_array . '</option>';
        }
     ?>
     </select>
--- web/results.php
+++ web/results.php
@@ -8,17 +8,17 @@ session_start();
 
         echo '<h1>These are your results in the database:</h1> <br><br>';
         
-        $first_name = $_POST['first_name_filter'];
+        $last_name = $_POST['last_name_filter'];
 
 
         //Prepared statement to get results filtered by book 
-        $stmt = $db->prepare('SELECT * FROM note WHERE first_name=:first_name');
-        $stmt->execute(array(':first_name' => $first_name));
+        $stmt = $db->prepare('SELECT * FROM note WHERE last_name=:last_name');
+        $stmt->execute(array(':last_name' => $last_name));
         $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         foreach ($rows as $row )
         {
-         echo '<a href=user_details.php?id=' . $row['id'] . '>' . $row['first_name'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</a>';
+         echo '<a href=user_details.php?id=' . $row['id'] . '>' . $row['first_name'] . ' ' . $row['last_name'] . ':' . $row['verse'] . '</a>';
          echo '<br><br>';
         }
  ?>
