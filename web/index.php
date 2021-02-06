a9494f2bc0916489f728498c14a83d23d001f1f4
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-05 21:16:57 -0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,46 +1,36 @@
-
+<?php include "sr1.php" ; ?>
 
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Management Page</title>
+        <title>Scriptures</title>
     </head>
     <body>
-        <h1>This Page is where we manage all the inputs and new items</h1>
+        <h1>Scripture Resources by Book</h1>
     </body>
 
-    <!-- <form method="post">
-     <label>Search</label>
-     <input type="text" name="search">
-     <input type="submit" name="submit">
-    
+    <form method="post" action="results.php">
+    <p>Search by:</p>
+    <label>Book</label>
     <select name='book_filter' id='book_filter' required>
     <?php
-       
-      if (isset($_POST["submit"])) 
-      {
-        $str = $_POST["search"];
-        $sth = $db->prepare("SELECT * FROM 'search' WHERE Name = '$str' ");
-        $sth->setFetchMode(PDO:: FETCH_ASSOC)
-        $sth->execute();
-        if($row = $sth->fetch())
-        {
-          ?>
-          <br><br><br>
-          <table>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-          </tr>
-          <tr>
-            <td><?php echo $row->Name; ?></th>
-            <td><?php echo $row->Description; ?></th>
-          </tr>
-          </table>
-        }
-      }
+       $books_array = array();
+       //Search for all the books in the DB 
+       foreach ($db->query ('SELECT DISTINCT book FROM scriptures') as $row )
+       {
+           //Add book to the book array if it has not been added 
+          if(!(in_array($row['book'], $books_array)))
+          {
+              array_push($books_array, $row['book']);
+          }
+       }
+       //loop the books_array variable to create an option for each book 
+       foreach ($books_array as $book_in_array )
+       {
+        echo '<option value="' . $book_in_array . '">' .  $book_in_array . '</option>';
+       }
     ?>
     </select>
     <button type="submit">Submit Form</button>
-    </form>  -->
+    <form> 
 </html>
