c87b486920f26c40c271e00538e9b4002389b905
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-05 23:08:35 -0700

    Update user_details.php

--- web/user_details.php
+++ web/user_details.php
@@ -6,31 +6,24 @@ session_start();
 <html>
 
 <?php
-    //Check if the page has been called adter a post method
-
         echo '<h1>user description</h1> <br><br>';
         
         $id = $_GET['id'];
 
-        //Prepared statement to get scripture details 
+      
         $stmt = $db->prepare('SELECT * FROM note WHERE id=:id');
         $stmt->execute(array(':id' => $id));
         $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         foreach ($rows as $row )
-        // {
-                    
-        //  echo '<b>' . $row['first_name'] . ' ' . $row['last_name'] . ':' . $row['userid'] . '</b>';
-        //  echo ' - "' . $row['content'] . '"'; 
-        //  echo '<br><br>';
-        // }
+     
  ?>
  <table>
     <tr>
-        <th>First Name  |</th><br>
-        <th>Last Name  |</th><br>
-        <th>User ID  |</th><br>
-        <th>Address  |</th><br>
+        <th>First Name  |</th>
+        <th>Last Name  |</th>
+        <th>User ID  |</th>
+        <th>Address  |</th>
     </tr>
     <tr>
         <td><?php echo $row['first_name']; ?></td>
