397f464f0aa0d33d47a8d4ca93335f38f59e4e32
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-05 23:45:27 -0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -7,7 +7,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
-    <form action="user_details.php">
+    <form action="user_search.php">
     <button type="submit" name="MANAGE" class="btn btn-info">MANAGE USERS</button>
     </form>
 </head>
