82d991604b91e84c838afcaeef7975eac1fc34b3
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-12 19:54:51 -0700

    New file

---
+++ web/signup.php
@@ -0,0 +1,14 @@
+<?php include "sr1.php" ; ?>
+<?php include("header.php"); 
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <form action="user_search.php">
+    <button type="submit" name="MANAGE" class="btn btn-info">MANAGE USERS</button>
+    </form>
+</head>
