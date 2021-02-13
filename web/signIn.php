6960a63ae600d6a2eeadce8faee1fabff2c95bb3
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-13 14:25:38 -0700

    New

--- web/signIn.php
+++ web/signIn.php
@@ -1,22 +1,7 @@
 
   
 <?php
-/**********************************************************
-* File: signIn.php
-* Author: Br. Burton
-* 
-* Description: This page has a form for the user to sign in.
-*
-* In this case, to show another approach, we will have this
-* page have two purposes, it will have the form for signing
-* in, but it will also have the logic to check a username
-* and password and redirect the user to the home page if
-* everything checks out. Thus it will post to itself.
-***********************************************************/
 
-// If you have an earlier version of PHP (earlier than 5.5)
-// You need to download and include password.php.
-//require("password.php"); // used for password hashing.
 session_start();
 
 $badLogin = false;
@@ -51,7 +36,7 @@ if (isset($_POST['txtUser']) && isset($_POST['txtPassword']))
 		{
 			// password was correct, put the user on the session, and redirect to home
 			$_SESSION['username'] = $username;
-			header("Location: home.php");
+			header("Location: index.php");
 			die(); // we always include a die after redirects.
 		}
 		else
