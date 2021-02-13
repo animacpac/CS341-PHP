bf11005c26f58a54bec61d79a94e730c9d477912
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-12 21:31:49 -0700

    New login

--- web/signup.php
+++ web/signup.php
@@ -1,3 +1,28 @@
 <?php include("header.php"); 
 session_start();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+
+<div class="container">
+    <div class="row">
+    <div class="col-md-6">
+        <h2> Sign Up Here </h2>
+        <form action="register.php" method="post">
+            <div class="form-group">
+                <label>Username </label>
+                <input type="text" name="user" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+                <label>Passowrd</label>
+                <input type="password" name="password" class="form-control" required>
+            </div>
+            <button type="subimit" class="btn btn-primary"> Register </button>
+        </form>
+    </div>
+</div>
+
+            
