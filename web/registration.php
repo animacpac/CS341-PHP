658090db5bec974b53b3da120b9c6650af95c893
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-13 11:38:38 -0700

    New

--- web/registration.php
+++ web/registration.php
@@ -1,5 +1,19 @@
 
 <?php 
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
     
 $showAlert = false;  
 $showError = false;  
--- web/signup.php
+++ web/signup.php
@@ -1,126 +1,140 @@
-
- <?php include 'registration.php'?>
-    
-<!doctype html> 
-    
-<html lang="en"> 
-  
-<head> 
-    
-    <!-- Required meta tags -->  
-    <meta charset="utf-8">  
-    <meta name="viewport" content= 
-        "width=device-width, initial-scale=1,  
-        shrink-to-fit=no"> 
-    
-    <!-- Bootstrap CSS -->  
-    <link rel="stylesheet" href= 
-"https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity= 
-"sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-        crossorigin="anonymous">   
-</head> 
-    
-<body> 
-    
-<?php 
-    
-    if($showAlert) { 
-    
-        echo ' <div class="alert alert-success  
-            alert-dismissible fade show" role="alert"> 
-    
-            <strong>Success!</strong> Your account is  
-            now created and you can login.  
-            <button type="button" class="close"
-                data-dismiss="alert" aria-label="Close">  
-                <span aria-hidden="true">×</span>  
-            </button>  
-        </div> ';  
-    } 
-    
-    if($showError) { 
-    
-        echo ' <div class="alert alert-danger  
-            alert-dismissible fade show" role="alert">  
-        <strong>Error!</strong> '. $showError.'
-    
-       <button type="button" class="close" 
-            data-dismiss="alert aria-label="Close"> 
-            <span aria-hidden="true">×</span>  
-       </button>  
-     </div> ';  
-   } 
+<?php
+// Include config file
+require_once "sr1.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Validate username
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter a username.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE username = ?";
         
-    if($exists) { 
-        echo ' <div class="alert alert-danger  
-            alert-dismissible fade show" role="alert"> 
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = trim($_POST["username"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $username_err = "This username is already taken.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
     
-        <strong>Error!</strong> '. $exists.'
-        <button type="button" class="close" 
-            data-dismiss="alert" aria-label="Close">  
-            <span aria-hidden="true">×</span>  
-        </button> 
-       </div> ';  
-     } 
-   
+    // Validate password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+        
+        // Prepare an insert statement
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            
+            // Set parameters
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Redirect to login page
+                header("location: login.php");
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+    
 ?> 
     
-<div class="container my-4 "> 
+<!DOCTYPE html>
+<html lang="en"> 
+  
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
     
-    <h1 class="text-center">Signup Here</h1>  
-    <form action="registration.php" method="post"> 
-    
-        <div class="form-group">  
-            <label for="username">Username</label>  
-        <input type="text" class="form-control" id="username"
-            name="username" aria-describedby="emailHelp">     
-        </div> 
-    
-        <div class="form-group">  
-            <label for="password">Password</label>  
-            <input type="password" class="form-control"
-            id="password" name="password">  
-        </div> 
-    
-        <div class="form-group">  
-            <label for="cpassword">Confirm Password</label>  
-            <input type="password" class="form-control"
-                id="cpassword" name="cpassword"> 
-    
-            <small id="emailHelp" class="form-text text-muted"> 
-            Make sure to type the same password 
-            </small>  
-        </div>       
-    
-        <button type="submit" class="btn btn-primary"> 
-        SignUp 
-        </button>  
-    </form>  
-</div> 
-    
-<!-- Optional JavaScript -->  
-<!-- jQuery first, then Popper.js, then Bootstrap JS --> 
-    
-<script src=" 
-https://code.jquery.com/jquery-3.5.1.slim.min.js" 
-    integrity=" 
-sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" 
-    crossorigin="anonymous"> 
-</script> 
-    
-<script src=" 
-https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" 
-    integrity= 
-"sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" 
-    crossorigin="anonymous"> 
-</script> 
-    
-<script src=" 
-https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"  
-    integrity= 
-"sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
-    crossorigin="anonymous"> 
-</script>  
-</body>  
-</html> 
+<body>
+    <div class="wrapper">
+        <h2>Sign Up</h2>
+        <p>Please fill this form to create an account.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                <label>Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="reset" class="btn btn-default" value="Reset">
+            </div>
+            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>  
+ 
