6e9d7778a337c5e4b416c9c90cdaa8ccef8340e6
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-15 17:07:47 -0700

    Fix

--- web/signIn.php
+++ web/signIn.php
@@ -53,18 +53,9 @@ if (isset($_POST['txtUser']) && isset($_POST['txtPassword']))
 // should just see the login form.
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
-</head>
-<div>
+<?php include("header.php"); 
+session_start();
+?>
 
 <?php
 if ($badLogin)
