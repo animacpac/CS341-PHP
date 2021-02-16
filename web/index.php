12a7244251ef746d8e38c171283f42b4bdcf7e18
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-15 17:32:37 -0700

    NEw

--- web/index.php
+++ web/index.php
@@ -1,4 +1,27 @@
 
+  
+<?php
+/**********************************************************
+* File: home.php
+* Author: Br. Burton
+* 
+* Description: This is the home page. It checks that a user
+*  exists on the session and redirects to the login page
+*  if it does not.
+***********************************************************/
+session_start();
+
+if (isset($_SESSION['username']))
+{
+	$username = $_SESSION['username'];
+}
+else
+{
+	header("Location: signIn.php");
+	die(); // we always include a die after redirects.
+}
+
+?>
 
 <?php include("header.php"); 
 session_start();
