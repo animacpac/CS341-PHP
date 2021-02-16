450c842f532800c0399e2793513007519ec57ccc
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-15 18:38:58 -0700

    submit

--- web/signup.php
+++ web/signup.php
@@ -32,10 +32,7 @@
                 <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
                 <span class="help-block"><?php echo $confirm_password_err; ?></span>
             </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <input type="reset" class="btn btn-default" value="Reset">
-            </div>
+
             <br>Please Put in your address<br>
             <div class="form-group <?php echo (!empty($streetname_err)) ? 'has-error' : ''; ?>">
                 <label>Street Name</label>
