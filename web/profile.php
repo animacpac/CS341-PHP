c4981036f5458b3fb2a581acb40953e2fa98b19a
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-19 23:06:18 -0700

    test

--- web/profile.php
+++ web/profile.php
@@ -1,12 +1,15 @@
 <?php include("header.php"); 
 session_start();
 $username = $_SESSION['username'];
-$current_user = $_SESSION['username'];
+
+?>
+
+<?php 
+    session_start();
+    $current_user = $_SESSION['username'];
     $username = mysqli_real_escape_string($database,$_REQUEST['username']);
     $profile_username=$rws['username'];
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <html lang="en-us" xmlns="http://www.w3.org/1999/xhtml">
@@ -27,5 +30,70 @@ $current_user = $_SESSION['username'];
                                 <div class="col-md-8 column">
                                     <p class="text-center profile-title"><i class="fa fa-info"></i> Basic</p>
                                     <hr>
+                                    <?php
+    if ($rws['street_name']){
+?>   
+                                    <div class="col-md-4">
+                                        <p class="profile-details"><i class="fa fa-info"></i> Bio</p>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p><?php echo $rws['street_name'];?></p>
+                                    </div>
+<?php } ?>
+<?php
+    if ($rws['city_name']){
+?>   
+                                    <div class="col-md-4">
+                                        <p class="profile-details"><i class="fa fa-info"></i> Location</p>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p><?php echo $rws['city_name'];?></p>
+                                    </div>
+<?php } ?>
+<?php
+    if ($rws['zipcode']){
+?>   
+                                    <div class="col-md-4">
+                                        <p class="profile-details"><i class="fa fa-envelope"></i> Email</p>
+                                    </div>
+                                    <div class="col-md-8">                                    
+                                        <p><?php echo $rws['zipcode'];?></p>
+                                    </div>
+<?php } ?>
+<?php
+    if ($rws['fname']){
+?>   
+                                    <div class="col-md-4">
+                                        <p class="profile-details"><i class="fa fa-info"></i> Country</p>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p><?php echo $rws['fname'];?></p>
+                                    </div>
+<?php } ?>
+                                </div>
+                                <div class="col-md-4 column">
+                                    <p class="text-center profile-title"><i class="fa fa-info"></i> Personal</p>
+                                    <hr>
+<?php
+    if ($rws['lname']){
+?>   
+                                    <div class="col-md-4">
+                                        <p class="profile-details"><i class="fa fa-user"></i> Gender</p>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p><?php echo $rws['lname'];?></p>
+                                    </div>
+<?php } ?>
+<?php
+   
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+		<!-- </div> -->
+	</div>
+</div>
 </body>
 </html>
