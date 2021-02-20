fe7ba5c27095cf051457c261554c4265fc433deb
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-19 23:01:41 -0700

    test

--- web/profile.php
+++ web/profile.php
@@ -1,14 +1,12 @@
 <?php include("header.php"); 
 session_start();
 $username = $_SESSION['username'];
-?>
-
-<?php 
-    session_start();
-    $current_user = $_SESSION['username'];
+$current_user = $_SESSION['username'];
     $username = mysqli_real_escape_string($database,$_REQUEST['username']);
     $profile_username=$rws['username'];
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <html lang="en-us" xmlns="http://www.w3.org/1999/xhtml">
