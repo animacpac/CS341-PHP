8d2926c8a7ca0cd945247274ac88007be1474d9c
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-19 23:08:45 -0700

    test

--- web/profile.php
+++ web/profile.php
@@ -30,61 +30,8 @@ $username = $_SESSION['username'];
                                 <div class="col-md-8 column">
                                     <p class="text-center profile-title"><i class="fa fa-info"></i> Basic</p>
                                     <hr>
-                                    <?php
-    if ($rws['street_name']){
-?>   
-                                    <div class="col-md-4">
-                                        <p class="profile-details"><i class="fa fa-info"></i> Bio</p>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <p><?php echo $rws['street_name'];?></p>
-                                    </div>
-<?php } ?>
-<?php
-    if ($rws['city_name']){
-?>   
-                                    <div class="col-md-4">
-                                        <p class="profile-details"><i class="fa fa-info"></i> Location</p>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <p><?php echo $rws['city_name'];?></p>
-                                    </div>
-<?php } ?>
-<?php
-    if ($rws['zipcode']){
-?>   
-                                    <div class="col-md-4">
-                                        <p class="profile-details"><i class="fa fa-envelope"></i> Email</p>
-                                    </div>
-                                    <div class="col-md-8">                                    
-                                        <p><?php echo $rws['zipcode'];?></p>
-                                    </div>
-<?php } ?>
-<?php
-    if ($rws['fname']){
-?>   
-                                    <div class="col-md-4">
-                                        <p class="profile-details"><i class="fa fa-info"></i> Country</p>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <p><?php echo $rws['fname'];?></p>
-                                    </div>
-<?php } ?>
-                                </div>
-                                <div class="col-md-4 column">
-                                    <p class="text-center profile-title"><i class="fa fa-info"></i> Personal</p>
-                                    <hr>
-<?php
-    if ($rws['lname']){
-?>   
-                                    <div class="col-md-4">
-                                        <p class="profile-details"><i class="fa fa-user"></i> Gender</p>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <p><?php echo $rws['lname'];?></p>
-                                    </div>
-<?php } ?>
-<?php
+                                    
+
    
                                 </div>
                             </div>
