fd00a4f457f0086f5f3ae66ba3e2cc147f3808ec
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-19 23:11:35 -0700

    test

--- web/profile.php
+++ web/profile.php
@@ -14,33 +14,9 @@ $username = $_SESSION['username'];
 <html lang="en">
 <html lang="en-us" xmlns="http://www.w3.org/1999/xhtml">
 <body>
-<div class="profile">
-	<div class="row clearfix">
-		<!-- <div class="col-md-12 column"> -->
-            <div>
-                
-                </center>
-                <h1 class="text-center profile-text profile-name"><?php echo $rws['fname'];?> <?php echo $rws['lname'];?></h1>
-                <h2 class="text-center profile-text profile-profession"><?php echo $rws['street_name'];?></h2>
-                <br>
-                <div class="panel-group white" id="panel-profile">
-                    <div class="panel panel-default">
-                        <div id="panel-element-info" class="panel-collapse collapse in">
-                            <div class="panel-body">
-                                <div class="col-md-8 column">
-                                    <p class="text-center profile-title"><i class="fa fa-info"></i> Basic</p>
-                                    <hr>
-                                    
+<h1><?php echo $row = ['fname']; ?></h1>
+	
 
-   
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-		<!-- </div> -->
-	</div>
-</div>
+
 </body>
 </html>
