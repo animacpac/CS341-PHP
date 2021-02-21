35da77932a4a04e8785527f55a2db581d724a3cf
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-20 21:40:02 -0700

    test

--- web/profile.php
+++ web/profile.php
@@ -1,24 +1,70 @@
-<?php require "dbConnect.php" ; ?>
+<?php
+/**********************************************************
+* File: showTopics.php
+* Author: Br. Burton
+* 
+* Description: This file retrieves the scriptures and topics
+* from the DB.
+***********************************************************/
+
+require("dbConnect.php");
+$db = get_db();
+
+?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>User Profile</title>
-    </head>
-    <body>
-        <?php
-            $stmt = $db->prepare("SELECT  id, fname, lname, street_name, city_name  FROM users");
-            $stmt->execute();
-            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            $content = '';
-            foreach ($rows as $row )
-            {
-               
-                echo '<br><br>' . $row['id'] . ' ' . $row['fname'] . ':' . $row['lname'] . '</b>';
-                echo ' - "' . $row['street_name'] . '"'; 
-                echo "Topics: " . $row['city_name'] . " ";
-                              
-                
-            }
-        ?>
-    </body>
+<head>
+	<title>Scripture and Topic List</title>
+</head>
+
+<body>
+<div>
+
+<h1>Scripture and Topic List</h1>
+
+<?php
+
+
+try
+{
+	// For this example, we are going to make a call to the DB to get the scriptures
+	// and then for each one, make a separate call to get its topics.
+	// This could be done with a single query (and then more processing of the resultset
+	// afterward) as follows:
+
+	//	$statement = $db->prepare('SELECT book, chapter, verse, content, t.name FROM scripture s'
+	//	. ' INNER JOIN scripture_topic st ON s.id = st.scriptureId'
+	//	. ' INNER JOIN topic t ON st.topicId = t.id');
+
+
+	// prepare the statement
+	$statement = $db->prepare('SELECT id, fname, lname, street_name, city_name FROM users');
+	$statement->execute();
+
+	// Go through each result
+	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+	{
+		echo '<p>';
+		echo '<strong>' . $row['fname'] . ' ' . $row['lname'] . ':';
+		echo $row['street_name'] . '</strong>' . ' - ' . $row['city_name'];
+		echo '<br />';
+		echo 'Topics: ';
+
+		// get the topics now for this scripture
+		
+	}
+
+
+}
+catch (PDOException $ex)
+{
+	echo "Error with DB. Details: $ex";
+	die();
+}
+
+?>
+
+</div>
+
+</body>
 </html>
