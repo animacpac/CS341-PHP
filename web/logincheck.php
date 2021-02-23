848a7cef834f9a40a0b5e7347dfc9d4a42b117c1
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-22 21:13:22 -0700

    test

--- web/logincheck.php
+++ web/logincheck.php
@@ -1,22 +1,51 @@
 <?php
-include("dbConnect.php");
+
 session_start();
-if($_SERVER["REQUEST_METHOD"] == "POST") {
- $username=mysqli_real_escape_string($con,$_POST['username']);
- $password=mysqli_real_escape_string($con,$_POST['password']);
- $result = mysqli_query($con,"SELECT * FROM users");
- $c_rows = mysqli_num_rows($result);
- if ($c_rows!=$username) {
-  header("location: signin.php?remark_login=failed");
- }
- $sql="SELECT id FROM users WHERE username='$username' and password='$password'";
- $result=mysqli_query($con,$sql);
- $row=mysqli_fetch_array($result);
- $active=$row['active'];
- $count=mysqli_num_rows($result);
- if($count==1) {
-  $_SESSION['login_user']=$username;
-  header("location: profile.php");
- }
+
+$badLogin = false;
+
+
+if (isset($_POST['txtUser']) && isset($_POST['txtPassword']))
+{
+	
+	$username = $_POST['txtUser'];
+	$password = $_POST['txtPassword'];
+
+	require("dbConnect.php");
+	$db = get_db();
+
+	$query = 'SELECT password FROM users WHERE username=:username';
+
+	$statement = $db->prepare($query);
+	$statement->bindValue(':username', $username);
+
+	$result = $statement->execute();
+
+	if ($result)
+	{
+		$row = $statement->fetch();
+		$hashedPasswordFromDB = $row['password'];
+
+
+		
+		if (password_verify($password, $hashedPasswordFromDB))
+		{
+			$_SESSION['login_user'] = $username;
+			header("Location: index.php");
+			
+			die(); 
+		}
+		else
+		{
+			$badLogin = true;
+		}
+
+	}
+	else
+	{
+		$badLogin = true;
+	}
 }
+
+
 ?>
