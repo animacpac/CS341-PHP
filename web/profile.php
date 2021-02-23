78f4ef1bb1b46edee04f9e9cc349b82ecbf1b95b
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-22 23:17:36 -0700

    test

--- web/profile.php
+++ web/profile.php
@@ -14,9 +14,16 @@ $username=$_SESSION['username'];
 <body>
 <?php
 echo $username
+$query = "SELECT fname, lname, street_name, city_name, zipcode FROM master WHERE id = '$username' ";
+$result = mysql_query($query);
+$row=mysql_fetch_array($result);
+while($row)
+ {
+  $uid=$row['fname'];
 
+  echo $uid;
 
-
+   }
 ?>
 
 
