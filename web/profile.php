0ae212a6811163907602653d8962f1f7da8af62f
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-22 20:24:40 -0700

    yes

--- web/profile.php
+++ web/profile.php
@@ -1,75 +1,74 @@
-
-<?php include("header.php"); 
-session_start();
-$username = $_SESSION['username'];
-?>
 <?php
-/**********************************************************
-* File: showTopics.php
-* Author: Br. Burton
-* 
-* Description: This file retrieves the scriptures and topics
-* from the DB.
-***********************************************************/
-
-require("dbConnect.php");
-$db = get_db();
-
+ include('signIn.php');
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Profile</title>
+ <meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
+ <link rel="stylesheet" type="text/css" href="style.css" />
+ <title>7topics - Login Demo</title>
 </head>
-
 <body>
-<div>
-
-<h1>Profile</h1>
-
+<header>
+ <nav>
+  <ul>
+   <li><a href="https://7topics.com">7topics</a></li>
+   <li><a href="https://7topics.com/creating-user-profile-page-using-php-and-mysql.html">Tutorial</a></li>
+  </ul>
+ </nav>
+</header>
+<div id="center">
+<div id="center-set"> -written by Rahul Ranjan
+<h1 align='center'>Welcome <?php echo $loggedin_session; ?>,</h1>
+You are now logged in. you can logout by clicking on signout link given below.
+<div id="contentbox">
 <?php
-
-
-try
-{
-	// For this example, we are going to make a call to the DB to get the scriptures
-	// and then for each one, make a separate call to get its topics.
-	// This could be done with a single query (and then more processing of the resultset
-	// afterward) as follows:
-
-	//	$statement = $db->prepare('SELECT book, chapter, verse, content, t.name FROM scripture s'
-	//	. ' INNER JOIN scripture_topic st ON s.id = st.scriptureId'
-	//	. ' INNER JOIN topic t ON st.topicId = t.id');
-
-
-	// prepare the statement
-	$statement = $db->prepare('SELECT id, fname, lname, street_name, city_name FROM users');
-	$statement->execute();
-
-	// Go through each result
-	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-	{
-		echo '<p>';
-		echo '<strong>' . $row['fname'] . ' ' . $row['lname'] . ':';
-		echo $row['street_name'] . '</strong>' . ' - ' . $row['city_name'];
-		echo '<br />';
-		echo 'Topics: ';
-
-		// get the topics now for this scripture
-		
-	}
-
-
-}
-catch (PDOException $ex)
-{
-	echo "Error with DB. Details: $ex";
-	die();
-}
-
+$sql="SELECT * FROM users where id=$loggedin_id";
+$result=mysqli_query($con,$sql);
 ?>
-
+<?php
+while($rows=mysqli_fetch_array($result)){
+?>
+<div id="signup">
+<div id="signup-st">
+<form action="" method="POST" id="signin" id="reg">
+<div id="reg-head" class="headrg">Your Profile</div>
+<table border="0" align="center" cellpadding="2" cellspacing="0">
+<tr id="lg-1">
+<td class="tl-1"> <div align="left" id="tb-name">Reg id:</div> </td>
+<td class="tl-4"><?php echo $rows['id']; ?></td>
+</tr>
+<tr id="lg-1">
+<td class="tl-1"><div align="left" id="tb-name">Username:</div></td>
+<td class="tl-4"><?php echo $rows['username']; ?></td>
+</tr>
+<tr id="lg-1">
+<td class="tl-1"><div align="left" id="tb-name">Name:</div></td>
+<td class="tl-4"><?php echo $rows['fname']; ?> <?php echo $rows['lname']; ?></td>
+</tr>
+<tr id="lg-1">
+<td class="tl-1"><div align="left" id="tb-name">Email id:</div></td>
+<td class="tl-4"><?php echo $rows['street_name']; ?></td>
+</tr>
+</table>
+<div id="reg-bottom" class="btmrg">Copyright &copy; 2015 7topics.com</div>
+</form>
 </div>
-
+</div>
+<div id="login">
+<div id="login-sg">
+<div id="st"><a href="logout.php" id="st-btn">Sign Out</a></div>
+<div id="st"><a href="deleteac.php" id="st-btn">Delete Account</a></div>
+</div>
+</div>
+<?php 
+// close while loop 
+}
+?>
+</div>
+</div>
+</div>
+</br>
+<div id="footer"><p> Copyright &copy; 2014-2015 7topics.com </p></div>
 </body>
 </html>
