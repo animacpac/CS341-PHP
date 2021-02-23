9e23be61e66bc802b3feb881a28f04412954cf8a
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-22 22:03:10 -0700

    test

--- web/profile.php
+++ web/profile.php
@@ -1,52 +1,35 @@
 <?php include("header.php"); 
 session_start();
 $username = $_SESSION['username'];
+
+$qry=mysqli_query($mysqli,"select * from Table users where username='$username'");
 ?>
   
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Scripture and Topic List</title>
+	<title>Profile</title>
 </head>
 
 <body>
 <div>
 
-<h1>Scripture and Topic List</h1>
-
+<h1>Show Profile</h1>
 <?php
-
-
-try
-{
-
-
-
-	
-	$statement = $username->prepare('SELECT id, username, fname, lname, street_name FROM users');
-	$statement->execute();
-
-	// Go through each result
-	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-	{
-		echo '<p>';
-		echo '<strong>' . $row['username'] . ' ' . $row['fname'] . ':';
-		echo $row['lname'] . '</strong>' . ' - ' . $row['street_name'];
-		echo '<br />';
-		
-		
+	$show = mysqli_query($mysqli,"SELECT * FROM `users` WHERE `id`='".$_SESSION['username'];."'");
+	while($row = mysqli_fetch_row($show)){
+?>
+	<tr class="even pointer">
+		<td><?= $row['username'] ?></td>
+		<td><?= $row['fname'] ?></td>
+		<td><?= $row['lname'] ?></td>
+		<td><?= $row['street_name'] ?></td>
+		<td><?= $row['city_name'] ?></td>
+	</tr>
+<?php 
 	}
 
-
-}
-catch (PDOException $ex)
-{
-	echo "Error with DB. Details: $ex";
-	die();
-}
-
-?>
 
 </div>
 
