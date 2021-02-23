c56a9f3b51553507ca6a970f5e191284e9f1865d
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-22 21:53:06 -0700

    test

--- web/profile.php
+++ web/profile.php
@@ -2,71 +2,68 @@
 session_start();
 $username = $_SESSION['username'];
 ?>
+  
+<?php
+
+
+require("dbConnect.php");
+$db = get_db();
+
+?>
 <!DOCTYPE html>
 <html>
+<head>
+	<title>Scripture and Topic List</title>
+</head>
 
 <body>
+<div>
 
-<div id="center">
-<h1 align='center'>Welcome <?= $username ?>,</h1>
-You are now logged in. you can logout by clicking on signout link given below.
-<div id="contentbox">
+<h1>Scripture and Topic List</h1>
+
 <?php
 
 
+try
+{
 
 
-	$statement = $db->prepare('SELECT id, fname, lname, street_name, city_name, zipcode FROM users');
+
+	
+	$statement = $db->prepare('SELECT id, username, fname, lname, street_name FROM users');
 	$statement->execute();
 
+	// Go through each result
+	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+	{
+		echo '<p>';
+		echo '<strong>' . $row['username'] . ' ' . $row['fname'] . ':';
+		echo $row['lname'] . '</strong>' . ' - ' . $row['street_name'];
+		echo '<br />';
+		
+
+		$stmtTopics = $db->prepare('SELECT name FROM topic t'
+			. ' INNER JOIN scripture_topic st ON st.topicId = t.id'
+			. ' WHERE st.scriptureId = :scriptureId');
+
+		$stmtTopics->bindValue(':scriptureId', $row['id']);
+		$stmtTopics->execute();
+
+	
+		
+	}
 
 
-
-
-?>
-<?php
-while($username=$username($PDO::FETCH_ASSOC)){
-?>
-<div id="signup">
-<div id="signup-st">
-<form action="" method="POST" id="signin" id="reg">
-<div id="reg-head" class="headrg">Your Profile</div>
-<table border="0" align="center" cellpadding="2" cellspacing="0">
-<tr id="lg-1">
-<td class="tl-1"> <div align="left" id="tb-name">Reg id:</div> </td>
-<td class="tl-4"><?php echo $rows['id']; ?></td>
-</tr>
-<tr id="lg-1">
-<td class="tl-1"><div align="left" id="tb-name">Username:</div></td>
-<td class="tl-4"><?php echo $rows['username']; ?></td>
-</tr>
-<tr id="lg-1">
-<td class="tl-1"><div align="left" id="tb-name">Name:</div></td>
-<td class="tl-4"><?php echo $rows['fname']; ?> <?php echo $rows['lname']; ?></td>
-</tr>
-<tr id="lg-1">
-<td class="tl-1"><div align="left" id="tb-name">Street Name: </div></td>
-<td class="tl-4"><?php echo $rows['street_name']; ?></td>
-</tr>
-</table>
-
-</form>
-</div>
-</div>
-<div id="login">
-<div id="login-sg">
-<div id="st"><a href="logout.php" id="st-btn">Sign Out</a></div>
-<div id="st"><a href="deleteac.php" id="st-btn">Delete Account</a></div>
-</div>
-</div>
-<?php 
-// close while loop 
 }
+catch (PDOException $ex)
+{
+	echo "Error with DB. Details: $ex";
+	die();
+}
+
 ?>
+
 </div>
-</div>
-</div>
-</br>
 
 </body>
 </html>
