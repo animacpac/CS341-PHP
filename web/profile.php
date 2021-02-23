8aab476ce326f5735c812befe95ae3b33ee1ebf1
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-22 22:16:54 -0700

    test

--- web/profile.php
+++ web/profile.php
@@ -1,5 +1,5 @@
 <?php include("header.php"); 
-include("dbConnect.php")
+include("dbConnect.php");
 session_start();
 $qry=mysqli_query($mysqli,"SELECT * FROM users WHERE username='".$_SESSION['username'];."'");
 ?>
