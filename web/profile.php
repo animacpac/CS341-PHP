57066cdfb27ad348ce009fa272d9f40ff5d49a31
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-22 23:12:21 -0700

    test1

--- web/profile.php
+++ web/profile.php
@@ -1,10 +1,7 @@
 <?php include("header.php"); 
+include "dbConnect.php";
 session_start();
 $username=$_SESSION['username'];
-require("dbConnect");
-
-$db = get_db();
-
 ?>
   
 
@@ -16,31 +13,7 @@ $db = get_db();
 
 <body>
 <?php
-
-
-try
-{
-	
-	$statement = $db->prepare('SELECT id, fname, lname, street_name, city_name FROM users');
-	$statement->execute();
-
-	// Go through each result
-	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-	{
-		echo '<p>';
-		echo '<strong>' . $row['fname'] . ' ' . $row['lname'] . ':';
-		echo $row['street_name'] . '</strong>' . ' - ' . $row['city_name'];
-		
-		echo '</p>';
-	}
-
-
-}
-catch (PDOException $ex)
-{
-	echo "Error with DB. Details: $ex";
-	die();
-}
+echo $username
 
 
 
