c9f3ee9962adc62bdb8a43a3e2283f43d5d8acd7
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-24 18:14:44 -0700

    new update

--- web/profile.php
+++ web/profile.php
@@ -19,13 +19,14 @@ include "dbConnect.php";
 
 $stmt = $db->prepare('SELECT id, username, fname, lname, street_name, city_name FROM users WHERE username=:username');
 $stmt->execute(array(':username' => $username));
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($rows as $row )
-        {
-         echo '<strong>Username:</strong>     ' . $row['username'] . '<p><strong>Full Name:</strong>     ' . $row['fname'] . ' ' . $row['lname'] . '<p><strong>Address:</strong>     ' . $row['street_name'] . '</br>'. $row['city_name'].'</a>';
-         echo '<br><br>';
-        }
+
+
+
+while($row = $stmt->fetch(PDO::FETCH_ASSOC))
+{
+    echo $stmt['fname'];
+}
 ?>
 
 
