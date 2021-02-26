32455f24ce029d7b410d7416ccc3273ff3e2cf9d
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-25 20:54:59 -0700

    test

--- web/profile.php
+++ web/profile.php
@@ -27,7 +27,9 @@ $stmt->execute(array(':username' => $username));
 while($row = $stmt->fetch(PDO::FETCH_ASSOC))
 {
     echo '</br><br>'.'Welcome: '. $row['fname'];
-    echo '</br><br>'.'<h3>First Name: </h3>'. $row['fname'] .'</br><br>'.'<h3>Last Name: </h3>'. $row['lname'];
+    echo '</br><br>'.'<h6>First Name </h6>'. $row['fname'] .'</br><br>'.'<h6>Last Name </h6>'.
+    $row['lname']. '</br><br>'.'<h6>Street Name </h6>'. 
+    $row['street_name'] .'</br><br>'.'<h6>City Name </h6>'. $row['city_name'].'<h6>Zipcode </h6>'. $row['zipcode'];
     
    
 
