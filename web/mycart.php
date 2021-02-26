3b62cbc8cfd871919499102b46d620665c05c45b
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-25 22:28:32 -0700

    test

--- web/mycart.php
+++ web/mycart.php
@@ -86,32 +86,32 @@ session_start();
                 <input type="text" id="state" name="state" placeholder="NY">
               </div>
               <div class="col-50">
-                <label for="zip">Zip</label>
-                <input type="text" id="zip" name="zip" placeholder="10001">
+                <label for="zipcode">Zip</label>
+                <input type="text" id="zipcode" name="zipcode" placeholder="10001">
               </div>
             </div>
           </div>
 
           <div class="col-50">
             <h3>Payment</h3>
-            <label for="fname">Accepted Cards</label>
+            <label for="fulname">Accepted Cards</label>
             <div class="icon-container">
               <i class="fa fa-cc-visa" style="color:navy;"></i>
               <i class="fa fa-cc-amex" style="color:blue;"></i>
               <i class="fa fa-cc-mastercard" style="color:red;"></i>
               <i class="fa fa-cc-discover" style="color:orange;"></i>
             </div>
-            <label for="cname">Name on Card</label>
-            <input type="text" id="cname" name="cardname" placeholder="John More Doe">
-            <label for="ccnum">Credit card number</label>
-            <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
+            <label for="name_card">Name on Card</label>
+            <input type="text" id="name_card" name="name_card" placeholder="John More Doe">
+            <label for="credit_card">Credit card number</label>
+            <input type="text" id="credit_card" name="credit_card" placeholder="1111-2222-3333-4444">
             <label for="expmonth">Exp Month</label>
-            <input type="text" id="expmonth" name="expmonth" placeholder="September">
+            <input type="text" id="exp_m" name="exp_m" placeholder="September">
 
             <div class="row">
               <div class="col-50">
-                <label for="expyear">Exp Year</label>
-                <input type="text" id="expyear" name="expyear" placeholder="2018">
+                <label for="exp_y">Exp Year</label>
+                <input type="text" id="exp_y" name="exp_y" placeholder="2018">
               </div>
               <div class="col-50">
                 <label for="cvv">CVV</label>
