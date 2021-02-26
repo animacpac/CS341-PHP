50ad19845e4ba50cb1db40d9f090769e6aa9f171
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-25 20:57:11 -0700

    zipcode

--- web/profile.php
+++ web/profile.php
@@ -18,7 +18,7 @@ $username=$_SESSION['username'];
 include "dbConnect.php";
 $db = get_db();
 
-$stmt = $db->prepare('SELECT id, username, fname, lname, street_name, city_name FROM users WHERE username=:username');
+$stmt = $db->prepare('SELECT id, username, fname, lname, street_name, city_name, zipcode FROM users WHERE username=:username');
 $stmt->execute(array(':username' => $username));
 
 
@@ -29,7 +29,7 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC))
     echo '</br><br>'.'Welcome: '. $row['fname'];
     echo '</br><br>'.'<h6>First Name </h6>'. $row['fname'] .'</br><br>'.'<h6>Last Name </h6>'.
     $row['lname']. '</br><br>'.'<h6>Street Name </h6>'. 
-    $row['street_name'] .'</br><br>'.'<h6>City Name </h6>'. $row['city_name'].'<h6>Zipcode </h6>'. $row['zipcode'];
+    $row['street_name'] .'</br><br>'.'<h6>City Name </h6>'. $row['city_name'].'</br><br>'.'<h6>Zipcode </h6>'. $row['zipcode'];
     
    
 
